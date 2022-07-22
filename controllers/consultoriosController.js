a53548da2615a0154491d0ed32c6d754e4844012
Mary <maia.mary1995@gmail.com>
2022-07-22 10:03:56 -0300

    1

--- controllers/consultoriosController.js
+++ controllers/consultoriosController.js
@@ -7,22 +7,19 @@ module.exports ={
         try {
             const { email, senha} = req.body;
             return res.json(req.body);
-<<<<<<< HEAD
 
             let senhaCript = bcrypt.hashSync(senha, 10);
-=======
         } catch (error) {
             console.log(error);
             res.status(500).json({error});
->>>>>>> e87c4d12aac3e0f004b9c4c99bd0352ec49d9ea4
         }
 
-        const novoConsultorio = await Consultorio.create({
-            CNPJ,
-            email,
-            senha,
-        });
-        res.send(novoConsultorio);
+       // const novoConsultorio = await Consultorio.create({
+       //     CNPJ,
+       //     email,
+        //    senha,
+        //});
+        //res.send(novoConsultorio);
 
     } 
 }
