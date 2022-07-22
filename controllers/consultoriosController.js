2c1465c8633b4229c623333d28a8973014709850
Mary <maia.mary1995@gmail.com>
2022-07-22 11:16:29 -0300

    Colocando o cadastrar pra funcionar

--- controllers/consultoriosController.js
+++ controllers/consultoriosController.js
@@ -1,25 +1,32 @@
-const {consultorio}=require('../models');
+const {Consultorio}=require('../models');
 const {Op} = require('sequelize');
 const bcrypt = require('bcrypt');
 
 module.exports ={
     cadastrar: async (req, res) => {
+        let senhaCript;
+        const {nome, email, senha, cep, uf, endereco, numero, telefone, complemento} = req.body;
         try {
-            const { email, senha} = req.body;
-            return res.json(req.body);
-
-            let senhaCript = bcrypt.hashSync(senha, 10);
+            
+            senhaCript = bcrypt.hashSync(senha, 10);
         } catch (error) {
             console.log(error);
             res.status(500).json({error});
         }
-
-       // const novoConsultorio = await Consultorio.create({
-       //     CNPJ,
-       //     email,
-        //    senha,
-        //});
-        //res.send(novoConsultorio);
+        
+        const novoConsultorio = await Consultorio.create({
+            nome,
+            email,
+            senha: senhaCript,
+            cep,
+            uf,
+            endereco,
+            numero,
+            telefone,
+            complemento
+        });
+        
+        res.send(novoConsultorio);
 
     } 
 }
