ff8502d75093ece06d9aba76f35714d7f6c2c65b
HugoDardis <hugolpd@gmail.com>
2022-07-25 08:46:08 -0300

    alterado login

--- controllers/petController.js
+++ controllers/petController.js
@@ -1,24 +1,35 @@
-const { pet } = require('../models');
+const {
+    pet
+} = require('../models');
 const bcrypt = require('bcrypt');
 
 
 module.exports = {
     cadastrarPet: async (req, res) => {
         const {
-          RGA,
-          nome,
-          raca,
-          tipoAnimal,
+            RGA,
+            nome,
+            raca,
+            tipoAnimal,
+            cliente_id: references_cliente_id = {
+                model: {
+                    tableName: "clientes"
+                },
+                key: "id"
+            }
         } = req.body;
-        
+
         const novoPet = await pet.create({
-          RGA,
-          nome,
-          raca,
-          tipoAnimal,
+            RGA,
+            nome,
+            raca,
+            tipoAnimal,
+            references_cliente_id
         });
         res.send(novoPet);
+        //return res.redirect('/home01');
     },
+
     listarPets: async (req, res) => {
         const pets = await pet.findAll();
         res.send(pets);
